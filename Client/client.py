e2ecdb3b9cd6aa8563f1a5454de9fc4f681c4d2b
dicemaster <dicemaster555@gmail.com>
2019-02-20 12:16:08 +0000

    new stuff?

--- Client/client.py
+++ Client/client.py
@@ -1,7 +1,281 @@
+import sys
 import socket
+import threading
+import time
+from PyQt5 import QtCore, QtGui, uic, QtWidgets
+
+clientIsRunning:bool = True
+newInput:str
+
+
+# ========================= PYQT WINDOW CODE ====================== #
+
+# Get UI file and load as window.
+qtCreatorFile = "DeisgnerWindow.ui"
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+
+# PyQT application.
+class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, game):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+
+        # Setup timer
+        #self.timer = QtCore.QTimer()
+        #self.timer.timeout.connect(self.timerEvent)
+        #self.timer.start(100)
+
+        # Send startup message
+        self.textDisplay.append("Window init.")
+
+        # button Onclick
+        self.InputButton.clicked.connect(lambda: self.EnterInputText())
+
+        # When enter is pressed in input box.
+        self.UserInputBox.returnPressed.connect(lambda: self.EnterInputText())
+
+
+# Test function to print stuff ====== REMOVE LATER
+    def PrintStuff(self):
+        print("Clicking button")
+
+    def EnterInputText(self):
+        self.newInput = self.UserInputBox.text().lower()
+        print("User input submitted")
+        print(self.newInput)
+
+        self.DisplayText(self.newInput)
+        self.UserInputBox.setText("")
+
+    def DisplayText(self, text):
+        self.textDisplay.append(text)
+
+
+# ========================= THREADING CODE ====================== #
+
+def sendThread(clientData):
+    print("sendThread running")
+
+
+def receiveThread(clientData):
+    print("receiveThread running")
+
+
+def backgroundThread(clientData):
+    print("backgroundThread running")
+
+
+# =================== MAIN ========================= #
+
+if __name__ == "__main__":
+    if clientIsRunning:
+        # Create qtApplication
+        app = QtWidgets.QApplication(sys.argv)
+
+        # Create and show qtWindow
+        window = MyApp(app)
+        window.show()
+
+        # main()
+
+        # Event loop
+        sys.exit(app.exec_())
+        clientIsRunning = False
+
+"""
+import sys
+import PyQt5.QtCore
+import PyQt5.QtWidgets
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5 import QtCore
+
+import socket
+import threading
 import time
 
-localUserInput: str = "First Time Connection"
+
+class ClientData:
+    def __init__(self):
+        self.serverSocket = None
+        self.connectedToServer = False
+        self.running = True
+        self.incomingMessage = ""
+        self.currentBackgroundThread = None
+        self.currentReceiveThread = None
+        self.userName = ""
+
+
+clientData = ClientData()
+clientDataLock = threading.Lock()
+
+
+def receiveThread(clientData):
+    print("receiveThread running")
+
+    while clientData.connectedToServer is True:
+        try:
+            data = clientData.serverSocket.recv(4096)
+            text = ""
+            text += data.decode("utf-8")
+            clientDataLock.acquire()
+            clientData.incomingMessage += text
+            clientDataLock.release()
+            print(text)
+        except socket.error:
+            print("Server lost")
+            clientData.connectedToServer = False
+            clientData.serverSocket = None
+
+
+def backgroundThread(clientData):
+    print("backgroundThread running")
+    clientData.connectedToServer = False
+
+    while (clientData.connectedToServer is False) and (clientData.running is True):
+        try:
+
+            if clientData.serverSocket is None:
+                clientData.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+            if clientData.serverSocket is not None:
+                clientData.serverSocket.connect(("127.0.0.1", 8222))
+
+            clientData.connectedToServer = True
+            clientData.currentReceiveThread = threading.Thread(target=receiveThread, args=(clientData,))
+            clientData.currentReceiveThread.start()
+
+            print("connected")
+
+            while clientData.connectedToServer is True:
+                time.sleep(1.0)
+
+        except socket.error:
+            print("no connection")
+            time.sleep(1)
+            clientDataLock.acquire()
+            clientData.incomingMessage = "\nNoServer"
+            clientDataLock.release()
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.chatOutput = 0
+        self.userInput = 0
+
+        self.initUI()
+
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.timerEvent)
+        self.timer.start(100)
+
+    def timerEvent(self):
+        if clientData.incomingMessage != "":
+            clientDataLock.acquire()
+            self.chatOutput.appendPlainText(clientData.incomingMessage)
+            clientData.incomingMessage = ""
+            clientDataLock.release()
+
+    def userInputOnUserPressedReturn(self):
+        entry = self.userInput.text()
+        print("User entry: "+entry)
+        clientData.serverSocket.send(bytes(entry, 'utf-8') )
+        self.userInput.setText("")
+
+    def initUI(self):
+        self.userInput = QLineEdit(self)
+        self.userInput.setGeometry(10, 360, 580, 30)
+        self.userInput.returnPressed.connect(self.userInputOnUserPressedReturn)
+
+        self.chatOutput = QPlainTextEdit(self)
+        self.chatOutput.setGeometry(10, 10, 580, 300)
+        self.chatOutput.setReadOnly(True)
+
+        self.setGeometry(300, 300, 600, 400)
+        self.setWindowTitle('Client Game Window')
+        self.show()
+
+    def closeEvent(self, event):
+
+        if clientData.serverSocket is not None:
+            clientData.serverSocket.close()
+            clientData.serverSocket = None
+            clientData.connectedToServer = False
+            clientData.running = False
+
+            if clientData.currentReceiveThread is not None:
+                clientData.currentReceiveThread.join()
+
+            if clientData.currentBackgroundThread is not None:
+                clientData.currentBackgroundThread.join()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    clientData.ex = Example()
+
+    clientData.currentBackgroundThread = threading.Thread(target=backgroundThread, args=(clientData,))
+    clientData.currentBackgroundThread.start()
+
+    sys.exit(app.exec_())
+"""
+"""
+import socket
+import time
+import threading
+
+from queue import *
+messageQueue = Queue()
+
+isRunning = True
+isConnected = False
+mySocket = None
+
+clientUserInput: str = "First Time Connection"
+
+def receiveThread(serverSocket):
+    global isConnected
+
+    while isRunning:
+        if isConnected:
+            try:
+                messageQueue.put(mySocket.recv(4096).decode("utf-8"))
+            except socket.error:
+                isConnected = False
+                print("lost server")
+        else:
+            print("no server")
+            time.sleep(5.0)
+
+
+if __name__ == '__main__':
+
+    mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    mySocket.connect(("127.0.0.1", 8222))
+    isConnected = True
+
+    myThread = threading.Thread(target=receiveThread, args=(mySocket,))
+    myThread.start()
+
+    testString = "this is a test from the python client"
+
+    mySocket.send(testString.encode())
+
+    while isRunning:
+        time.sleep(1.0)
+
+        while messageQueue.qsize()> 0:
+            print(messageQueue.get())
+
+        clientUserInput = input().lower()
+        mySocket.send(clientUserInput.encode())
+
 
 if __name__ == '__main__':
 
@@ -48,4 +322,5 @@ if __name__ == '__main__':
 
             except:
                 isConnected = False
-                mySocket = None
+                mySocket = None 
+"""
--- Server/server.py
+++ Server/server.py
@@ -1,5 +1,155 @@
+import sys
+import socket
+import threading
+
+from queue import *
+from commands import *
+
+
+"""
+import sys
+import socket
+import threading
+
+from queue import *
+from commands import *
+
+
+messageQueue = Queue()
+
+clientIndex = 0
+currentClients: dict = {}
+currentClientsLock = threading.Lock()
+
+
+class Player:
+    playerUsername: str = "NewUser"
+
+
+def clientReceive(clientsocket):
+    print("clientReceive running")
+    clientValid = True
+    while clientValid == True:
+        try:
+            data = clientsocket.recv(4096);
+
+            currentClientsLock.acquire()
+            msg = "client-" + str(currentClients[clientsocket]) + ":"
+            msg += data.decode("utf-8")
+            currentClientsLock.release()
+
+            print("received client msg:"+str(data, "utf-8"))
+
+            messageQueue.put(ClientMessage(clientsocket, msg))
+        except socket.error:
+            print("clientReceive - lost client")
+            clientValid = False
+            messageQueue.put(ClientLost(clientsocket))
+
+
+def acceptClients(serversocket):
+    print("acceptThread running")
+    while True:
+        (clientsocket, address) = serversocket.accept()
+        messageQueue.put(ClientJoined(clientsocket))
+
+        thread = threading.Thread(target=clientReceive, args=(clientsocket,))
+        thread.start()
+
+
+def handleClientLost(command):
+    currentClientsLock.acquire()
+    lostClient = currentClients[command.socket]
+    print("Removing lost client: client-"+str(lostClient))
+
+    for key in currentClients:
+        if key != command.socket:
+            key.send(bytes("client-"+str(lostClient) + " has left the chat room", 'utf-8'))
+
+    del currentClients[command.socket]
+
+    currentClientsLock.release()
+
+
+def handleClientJoined(command):
+    global clientIndex
+
+    currentClientsLock.acquire()
+    currentClients[command.socket] = clientIndex
+    clientIndex += 1
+
+    print("Client joined: client-" + str(currentClients[command.socket]))
+
+    outputToUser = "Welcome to chat! What is your Username?\n"
+
+    outputToUser += "You are: client-" + str(currentClients[command.socket]) +"\n"
+    outputToUser += "Present in chat:\n"
+
+    for key in currentClients:
+        outputToUser += "client-" + str(currentClients[key]) + "\n"
+
+    command.socket.send(bytes(outputToUser, 'utf-8'))
+
+    for key in currentClients:
+        if key != command.socket:
+            key.send(bytes("client-"+str(currentClients[command.socket]) + " has joined the chat room", 'utf-8'))
+
+    currentClientsLock.release()
+
+
+def handleClientMessage(command):
+    print("client message: "+command.message)
+
+    currentClientsLock.acquire()
+    for key in currentClients:
+        try:
+            key.send(bytes(command.message, 'utf-8'))
+        except socket.error:
+            messageQueue.put(ClientLost(key))
+
+    currentClientsLock.release()
+
+
+def main():
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        if len(sys.argv) > 1:
+            serversocket.bind((sys.argv[1], 8222))
+        else:
+            serversocket.bind(("127.0.0.1", 8222))
+    except socket.error:
+        print("Can't start server, is another instance running?")
+        exit()
+
+    serversocket.listen(5)
+
+    thread = threading.Thread(target=acceptClients,args=(serversocket,))
+    thread.start()
+
+    while True:
+
+        if messageQueue.qsize() > 0:
+            print("Processing client commands")
+            command = messageQueue.get()
+
+            if isinstance(command, ClientJoined):
+                handleClientJoined(command)
+
+            if isinstance(command, ClientLost):
+                handleClientLost(command)
+
+            if isinstance(command, ClientMessage):
+                handleClientMessage(command)
+
+
+if __name__ == "__main__":
+    main()
+"""
+
 import socket
 import time
+import threading
 
 #########
 playerName: str
@@ -7,7 +157,70 @@ userInput: str
 output: str = "null"
 client = 0
 
+serverIsRunning: bool = True
 
+clients = {}
+clientsLock = threading.Lock()
+
+
+########################THREADING CODE########################
+
+
+def acceptThread(serverSocket):
+    while True:
+        new_client = serverSocket.accept()
+        print("Added client!")
+        clientsLock.acquire()
+        clients[new_client[0]] = 0
+        clientsLock.release()
+
+def receiveThread(serverSocket):
+    while True:
+        #print("looking for message from client")
+        clientsLock.acquire()
+
+        clientsLock.release()
+
+if __name__ == '__main__':
+    mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    mySocket.bind(("127.0.0.1", 8222))
+    mySocket.listen(5)
+
+    myThread = threading.Thread(target=acceptThread, args=(mySocket, ))
+    myThread.start()
+
+    receivingThread = threading.Thread(target=receiveThread, args=(mySocket, ))
+    receivingThread.start()
+
+    while serverIsRunning:
+        lostclients = []
+
+        clientsLock.acquire()
+        for client in clients:
+            try:
+                testString = str(clients[client]) + ":" + time.ctime()
+                clients[client] += 1
+                client.send(testString.encode())
+
+                print("Sending: " + testString)
+
+                data = client.recv(4096)
+                print(data.decode("utf-8"))
+
+            except socket.error:
+                lostclients.append(client)
+                print("Lost client!")
+
+        for client in lostclients:
+            clients.pop(client)
+
+        clientsLock.release()
+
+        time.sleep(0.5)
+######################################
+
+"""
 def sendOutput(outPutMessage: str):
     print("Sending Output Message: " + "|" + outPutMessage + "|")
     client[0].send(outPutMessage.encode())
@@ -62,8 +275,6 @@ def idle():
         sendOutput("ERROR: incorrect input")
         idle()
 
-#########
-
 
 if __name__ == '__main__':
 
@@ -102,3 +313,4 @@ if __name__ == '__main__':
                 isConnected = False
                 client = None
                 print("Client lost")
+"""
